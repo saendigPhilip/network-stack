b43a4ce9fa7b56d2d7c291693324405a8a82a248
saendigPhilip <philip.saendig@web.de>
2021-05-18 22:08:16 +0200

    Bugfix: Client classified error code as old sequence number

--- client_server_twosided/src/Client.cpp
+++ client_server_twosided/src/Client.cpp
@@ -114,7 +114,8 @@ void Client::decrypt_cont_func(void *context, void *message_tag) {
     incoming_op = OP_FROM_SEQ_OP(incoming_header.seq_op);
     // This is most likely a message that has already been processed (timeout)
     // If it's not, it's a replay or similar, so we don't care about it
-    if (incoming_header.seq_op != NEXT_SEQ(tag->header.seq_op)) {
+    if ((incoming_header.seq_op & SEQ_MASK) !=
+            (NEXT_SEQ(tag->header.seq_op) & SEQ_MASK)) {
         cerr << "Message with old sequence number arrived" << endl;
         return;
     }
