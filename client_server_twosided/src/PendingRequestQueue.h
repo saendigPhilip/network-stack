af604b409cd7e329b5d7e14e0c6d8705da5537bc
saendigPhilip <philip.saendig@web.de>
2021-06-25 15:35:47 +0200

    Extended Queue size

--- client_server_twosided/src/PendingRequestQueue.h
+++ client_server_twosided/src/PendingRequestQueue.h
@@ -9,7 +9,7 @@
 #include "client_server_common.h"
 #include "sent_message_tag.h"
 
-static constexpr size_t MAX_ACCEPTED_RESPONSES = 64;
+static constexpr size_t MAX_ACCEPTED_RESPONSES = 4096;
 
 
 class PendingRequestQueue {
